b0c5e6e4bedd474b0fc72bb71a2e8aecdcd2911e
Mohammad Shahzad <shahzad201415@gmail.com>
2024-06-26 16:52:48 +0530

    changes

--- model/diagnosticLabs.js
+++ model/diagnosticLabs.js
@@ -1,7 +1,5 @@
-// models/DiagnosticLab.js
-const mongoose = require("mongoose");
-const DiagnosticTest = require("./DiagnosticTest"); // Import DiagnosticTest model
 
+const mongoose = require("mongoose");
 const DiagnosticLabSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
